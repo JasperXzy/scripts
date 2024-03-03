dd829e036260ba8780efcc94ec1e62d6f1e5fd14
JasperXzy <jasperxzy2006@gmail.com>
2024-03-03 23:20:31 +0800

    找到需要更改 DNS 的 NNR 服务器

--- nnr_cloudflare_ddns/main.py
+++ nnr_cloudflare_ddns/main.py
@@ -1,10 +1,26 @@
-
 import requests
 import settings
 
-# NNR 请求头
+# nnr 请求头
 nnr_headers = {"content-type": "application/json",
                "token": settings.nnr_api_token}
 
+# 通过 api 请求 nnr 服务器列表
 response = requests.post(url=settings.nnr_url, headers=nnr_headers)
 data = response.json()
+
+# 将 json 转为 python 对象
+data = data["data"]
+
+# 选取 name 为 target_name 的服务器
+target = [server for server in data if server["name"] == settings.target_name]
+
+# 选取 host ip
+target = target[0]["host"]
+
+# 创建新列表分割服务器 ip
+target = target.split(",")
+
+# 判断服务器 ip 数量
+ip_num = len(target)
+print(ip_num)
--- nnr_cloudflare_ddns/settings.py
+++ nnr_cloudflare_ddns/settings.py
@@ -4,3 +4,6 @@ nnr_url = 'https://nnr.moe/api/servers'
 # nnr api token
 nnr_api_token = ''
 
+# nnr 目标服务器
+target_name = ''
+
